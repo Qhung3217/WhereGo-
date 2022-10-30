5e1730e814b91cafa941dccee6a10d4e5057a379
Qhung <quanghung165200@gmail.com>
2022-10-30 15:12:09 +0700

    Logic for traveler profile page [processing]
    Complete bookings

--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
-import { TableBodyComponent } from './table-body/table-body.component';
-import { TableToolsComponent } from './table-tools/table-tools.component';
 import { SearchIconModule } from 'src/app/shared/icons/search-icon/search-icon.module';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChevronRightIconModule } from 'src/app/shared/icons/chevron-right-icon/chevron-right-icon.module';
 import { EditIconModule } from 'src/app/shared/icons/edit-icon/edit-icon.module';
 import { TrashIconModule } from 'src/app/shared/icons/trash-icon/trash-icon.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
-  declarations: [TableComponent, TableBodyComponent, TableToolsComponent],
+  declarations: [TableComponent],
   imports: [
     CommonModule,
+    FormsModule,
+
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
 
     SearchIconModule,
     NgbPaginationModule,
