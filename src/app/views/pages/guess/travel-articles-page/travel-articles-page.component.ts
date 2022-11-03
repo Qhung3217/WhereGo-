8631876ca6df2011dd8aecf9db741f71d67cb1dc
Qhung <quanghung165200@gmail.com>
2022-11-03 16:18:30 +0700

    Logic for travel article search [done]

--- src/app/views/pages/guess/travel-articles-page/travel-articles-page.component.ts
+++ src/app/views/pages/guess/travel-articles-page/travel-articles-page.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Params } from '@angular/router';
+import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
+import { Subscription } from 'rxjs';
 import { Article } from 'src/app/core/models/article.model';
 import { ArticleService } from 'src/app/core/services/article.service';
 import { ImageService } from 'src/app/core/services/image.service';
+import { SearchService } from 'src/app/core/services/search.service';
 
 @Component({
   selector: 'app-travel-articles-page',
@@ -13,19 +17,27 @@ export class TravelArticlesPageComponent implements OnInit {
   page: number = 1;
   articles: Article[] = [];
   isFetching = false;
+  articleSub!: Subscription;
+  keyword: string = '';
   constructor(
     private articleService: ArticleService,
-    public imageService: ImageService,
-    private title: Title
+    private title: Title,
+    private route: ActivatedRoute,
+    private searchService: SearchService,
+    public imageService: ImageService
   ) {}
 
   ngOnInit(): void {
     this.title.setTitle('Travel articles');
+    this.articleSub = this.route.queryParams.subscribe((params: Params) => {
+      this.keyword = params['keyword'];
+      console.log('list: ', this.keyword);
 
-    this.isFetching = true;
-    this.articleService.getAll().subscribe((articles) => {
-      this.isFetching = false;
-      this.articles = [...articles];
+      if (this.keyword) {
+        this.title.setTitle('Travel articles search: ' + this.keyword);
+        this.isFetching = true;
+        this.searchArticles(this.keyword);
+      } else this.fetchAllArticles();
     });
   }
   handlePageChange(page: any) {
@@ -34,6 +46,21 @@ export class TravelArticlesPageComponent implements OnInit {
       top: 0,
       left: 0,
       behavior: 'smooth',
+    });
+  }
+  private searchArticles(keyword: string) {
+    this.searchService.articleSearch(this.keyword).subscribe({
+      next: (res) => {
+        this.articles = [...(res as Article[])];
+        this.isFetching = false;
+      },
+      error: () => (this.isFetching = false),
+    });
+  }
+  private fetchAllArticles() {
+    this.articleService.getAll().subscribe((articles) => {
+      this.isFetching = false;
+      this.articles = [...articles];
     });
   }
 }
